6af993c045248d77ccd16b92e5ce116c61948757
Pieter van Beek <pieter@djinnit.com>
2012-08-16 17:12:06 +0200

    Removed all the stuff that has been put into the webdav-php repository.

--- scripts/mkalldocs.sh
+++ scripts/mkalldocs.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
 
-cd "$( dirname '$0' )"/..
+cd "$( dirname "$0" )/../"
 DIRNAME="$PWD"
 mkdir docs 2>/dev/null
-rm -rf docs/devel docs/user 2>/dev/null
+rm -rf docs/* 2>/dev/null
 
-
-mkdir docs/devel 2>/dev/null
 phpdoc \
-  --filename 'dav/*.php','sd/*.php' \
-  --ignore '*/.svn' \
-  --target "${DIRNAME}/docs/devel" \
-  --output HTML:frames:default \
-  --parseprivate on \
-  --sourcecode on \
-  --defaultpackagename DAV_Server \
-  --title "DAV_Server Documentation"
-exit
-
-mkdir docs/user 2>/dev/null
-phpdoc \
-  --filename 'dav/*.php','sd/*.php' \
-  --ignore '*/.svn/' \
-  --target "${DIRNAME}/docs/user" \
-  --output HTML:frames:default \
-  --parseprivate off \
-  --sourcecode on \
-  --defaultpackagename DAV_Server \
-  --title "DAV_Server Documentation"
+  --directory 'src,webdav-php/lib' \
+  --filename '*.php' \
+  --target "${DIRNAME}/docs" \
+  --parseprivate \
+  --sourcecode \
+  --defaultpackagename BeeHub \
+  --title "BeeHub Documentation"
