ca36ed137c34ceca8af2fa35db64f90b7c627f62
Niek Bosch <niek.bosch@sara.nl>
2013-01-15 19:40:36 +0100

    Updated index.php to define ENT_HTML5 in case it is not defined (PHP <5.4)

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 // Prepare the environment: where is the configuration file and are we in development or production mode?
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? strtolower(getenv('APPLICATION_ENV')) : 'production'));
+defined('ENT_HTML5') || define('ENT_HTML5', 0);
 
 // Then start the application
 require_once '../src/beehub_run.php';
