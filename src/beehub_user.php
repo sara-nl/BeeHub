0c87be75e6488ca1796b949be3663684cd39b509
Niek Bosch <niek.bosch@sara.nl>
2013-02-06 18:50:43 +0100

    When updating principal properties, a file (/system/js/principals.js) is created holding all displaynames of all principals

--- src/beehub_user.php
+++ src/beehub_user.php
@@ -230,6 +230,8 @@ class BeeHub_User extends BeeHub_Principal {
       die($p_verification_code);
     }
 
+    // Update the json file containing all displaynames of all privileges
+    self::update_principals_json();
     $this->touched = false;
   }
 
