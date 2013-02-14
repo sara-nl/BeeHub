95c4b60d7f034f17036a0d93cf7b3f8d8caed460
Laura Leistikow <laura@surfsara.nl>
2013-02-14 17:13:15 +0100

    beehub_groups

--- views/beehub_groups.php
+++ views/beehub_groups.php
@@ -8,53 +8,6 @@
   // wil maar dit kan waarschijnlijk netter/anders
   $header = '
 <style type="text/css">
-//   #joingroups, #membershiprequests {
-//     padding-left:30px;
-//   }
-
-//   .custom-accordion {
-//     border: 0px solid #E5E5E5 !important;
-//     border-style:dotted !important;
-//     border-top-width:1px !important;
-//     margin-bottom: 0px !important;
-//   }
-
-//   .custom-accordion-other-background{
-//     background-color: #E6E7E8 !important;
-//   }
-
-//   .custom-accordion:hover {
-//     background-color: #D1E2D3 !important;
-//     border-style:dotted !important;
-//   }
-
-//   .custom-accordion-inner {
-//     border-top: 1px solid #E5E5E5 !important;
-//   }
-
-//   .custom-accordion-active {
-//     background-color: #E8F1E9 !important;
-//     border: 1px solid #B9D3BA !important;
-//   }
-
-//   .custom-accordion-active:hover {
-//     background-color: #E8F1E9 !important;
-//     border-style:solid !important;
-//   }
-
-//   .admin{
-//     font-size: 12px !important;
-//     text-align: right;
-//   }
-
-//   .rightbutton{
-//     padding: 12px !important;
-//     text-align: right;
-//   }
-
-//   .custom-btn-primary {
-//     width: 130px !important;
-//   }
 
 </style>
   ';
@@ -73,7 +26,43 @@
 
 	<!-- My Groups tab -->
   <div id="panel-mygroups" class="tab-pane fade in active">
-      My groups
+		<!--   	List with my groups -->
+		<div class="accordion" id="membershipgroups">
+  	<?php
+    	$i = 1;
+     	foreach ($groups as $group) :
+//           if ( $group->is_member() ) :
+    	?>
+      	<div class="accordion-group">
+        	<div class="accordion-heading">
+          	<div class="accordion-toggle" data-toggle="collapse" data-parent="#membershipgroups" href="#membershipgroups-<?= $i ?>">
+            	<table width="100%"><tbody><tr>
+              	<th align="left"><?= $group->prop_displayname() ?></th>
+              	<td align="right">
+              		<!--   	View button (when not admin of the group) or manage button -->
+                	<?php if ( $group->is_admin() ) : ?>
+	                <a href="<?= $group->path ?>" class="btn btn-info">Manage</a>
+	                <?php else : ?>
+	                <a href="<?= $group->path ?>" class="btn">View</a>
+	                <?php endif; ?>
+	                <!--   	Leave button -->
+                	<button type="button" name="leave" value="<?= $this->user_prop_current_user_principal() ?>" class="btn btn-danger">Leave</button>
+	              </td>
+	            </tr></tbody></table>
+	          </div>
+	        </div>
+	        <div id="membershipgroups-<?= $i ?>" class="accordion-body collapse">
+
+        		<div class="accordion-inner">
+          		<?= $group->prop(BeeHub::PROP_DESCRIPTION) ?>
+	      		</div>
+	      	</div>
+	      </div>
+     <?php
+     	$i = $i + 1;
+//         endif;
+      	endforeach;
+     ?>        
   </div>
 	<!--   En my groups tab -->
 
@@ -85,7 +74,7 @@
 
 
 	<!-- Create tab -->
-	<div id="panel-join" class="tab-pane fade">
+	<div id="panel-create" class="tab-pane fade">
 		Create
   </div>
 	<!-- 	End create tab -->
