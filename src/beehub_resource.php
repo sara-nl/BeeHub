c87912d08c0b706008e40e9a6e2907776f1cca84
Niek Bosch <niek.bosch@surfsara.nl>
2013-12-03 16:25:21 +0100

    Fixed some ACL bugs

--- src/beehub_resource.php
+++ src/beehub_resource.php
@@ -164,14 +164,15 @@ abstract class BeeHub_Resource extends DAVACL_Resource {
       new DAVACL_Element_ace(
         'DAV: owner', false, array('DAV: all'), false, true, null
       ),
-      new DAVACL_Element_ace(
-        'DAV: all', false, array('DAV: unbind'), false, true, null
-      ),
     );
     if ( ('/' === $this->path) ||
          ('/home/' === $this->path) ){
       $protected[] = new DAVACL_Element_ace(
-        'DAV: all', false, array('DAV: read', 'DAV: read-acl'), false, true, null
+        'DAV: all', false, array( 'DAV: read' ), false, true, null
+      );
+    }else{
+      $protected[] = new DAVACL_Element_ace(
+        'DAV: all', false, array('DAV: unbind'), false, true, null
       );
     }
     $parent = $this->collection();
